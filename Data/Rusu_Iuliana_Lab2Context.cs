da259f514f1c45402a6c0b230e80f239f4dc9f25
User <User@DESKTOP-4BPP4H1>
2022-11-11 11:03:22 +0200

    Add project files.

---
+++ Data/Rusu_Iuliana_Lab2Context.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Rusu_Iuliana_Lab2.Models;
+
+namespace Rusu_Iuliana_Lab2.Data
+{
+    public class Rusu_Iuliana_Lab2Context : DbContext
+    {
+        public Rusu_Iuliana_Lab2Context (DbContextOptions<Rusu_Iuliana_Lab2Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Rusu_Iuliana_Lab2.Models.Book> Book { get; set; } = default!;
+
+        public DbSet<Rusu_Iuliana_Lab2.Models.Publisher> Publisher { get; set; }
+    }
+}
---
+++ Models/Publisher.cs
@@ -0,0 +1,11 @@
+﻿namespace Rusu_Iuliana_Lab2.Models
+{
+    public class Publisher
+    {
+        public int ID { get; set; }
+        public string PublisherName { get; set; } 
+        public ICollection<Book>? Books { get; set; } //navigation property
+    }
+
+}
+
